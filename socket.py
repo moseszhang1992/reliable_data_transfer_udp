257a245bd832476083942d083f1d66635d7e6df0
张宪哲（Xianzhe Zhang） <zhangxzh217@gmail.com>
2017-04-17 06:57:38 -0400

    Update socket.py

--- socket.py
+++ socket.py
@@ -1,15 +1,47 @@
-
 import binascii
 import socket as syssock
 import struct
 import sys
-import Queue
-import time
-import random
-import select
+
+import nacl.utils
+import nacl.secret
+import nacl.utils
+from nacl.public import PrivateKey, Box
+
+from inspect import currentframe, getframeinfo
+
+#global sock352portTx
+#global sock352portRx
+
+global publicKeysHex
+global privateKeysHex
+
+# the public and private keychains in binary format
+global publicKeys
+global privateKeys
+global in_publicKeys
+global local_box
+
+# the encryption flag
+global ENCRYPT
+
+publicKeysHex = {}
+privateKeysHex = {}
+publicKeys = {}
+privateKeys = {}
+
+# this is 0xEC
+ENCRYPT = 236
+
+sock352HdrStructStr = '!BBBBHHLLQQLL'
 
 
-def init(UDPportTx,UDPportRx):
+def init(UDPportTx, UDPportRx):
+    #global sock352portTx = local_port_for_sending
+    #global sock352portRx = local_port_for_receiving
+
+    # create the sockets to send and receive UDP packets on
+    # if the ports are not equal, create two sockets, one for Tx and one for Rx
     global sock
     global local_seq_number
     global local_ack_number
@@ -22,11 +54,75 @@ def init(UDPportTx,UDPportRx):
 
     sock = syssock.socket(syssock.AF_INET, syssock.SOCK_DGRAM)
     sock.bind(local_port_for_receiving)
-
+    #ADDING second socket
     print >> sys.stderr, 'Binding to UDPportRx ... %s' % UDPportRx
     pass
-    
-class socket(syssock.socket):
+
+# read the keyfile. The result should be a private key and a keychain of
+# public keys
+def readKeyChain(filename):
+    global publicKeysHex
+    global privateKeysHex
+    global publicKeys
+    global privateKeys
+
+    if (filename):
+        try:
+            keyfile_fd = open(filename, "r")
+            for line in keyfile_fd:
+                words = line.split()
+                # check if a comment
+                # more than 2 words, and the first word does not have a
+                # hash, we may have a valid host/key pair in the keychain
+                if ((len(words) >= 4) and (words[0].find("#") == -1)):
+                    host = words[1]
+                    port = words[2]
+                    keyInHex = words[3]
+                    if (words[0] == "private"):
+                        privateKeysHex[(host, port)] = keyInHex
+                        privateKeys[(host, port)] = nacl.public.PrivateKey(keyInHex, nacl.encoding.HexEncoder)
+                    elif (words[0] == "public"):
+                        publicKeysHex[(host, port)] = keyInHex
+                        publicKeys[(host, port)] = nacl.public.PublicKey(keyInHex, nacl.encoding.HexEncoder)
+        except Exception, e:
+            print ("error: opening keychain file: %s %s" % (filename, repr(e)))
+    else:
+        print ("error: No filename presented")
+
+    return (publicKeys, privateKeys)
+
+
+def __generate_encryption_keys(filename, UDPportTx, UDPportRx):
+    # global UDPportTxGl
+    # global UDPportRxGl
+
+    print 'UDPportTx: %s\n' % UDPportTx
+    print 'UDPportRx: %s\n' % UDPportRx
+
+    print ("DBG: Generating keys file")
+
+    local_privk = PrivateKey.generate()
+
+    privateKeysHex = local_privk.encode(encoder=nacl.encoding.HexEncoder)
+
+    # this is the local public key in binary
+    local_pubk = local_privk.public_key
+
+    # convert the binary key to a printable version in hexadecimal
+    publicKeysHex = local_pubk.encode(encoder=nacl.encoding.HexEncoder)
+    # generate the receiver's keychain file
+    keychainFile = open(filename, "w")
+    keychainFile.write('private\t*\t*\t%s\n' % privateKeysHex)
+    print type(UDPportTx)
+    keychainFile.write('public\t%s\t%s\t%s\n' % (syssock.gethostbyname(syssock.getfqdn()), UDPportTx, publicKeysHex))
+    if (UDPportTx != UDPportRx):
+        keychainFile.write(
+            'public\t%s\t%s\t%s\n' % (syssock.gethostbyname(syssock.getfqdn()), UDPportRx, publicKeysHex))
+
+    return
+
+
+class socket:
     global seq_number
     seq_number = 0
     global ack_number
@@ -40,11 +136,25 @@ class socket(syssock.socket):
         fragment_list = Queue.Queue()
         return
 
+
     def bind(self, address):
         print >> sys.stderr, '\nRUNNING instance bind() ... '
-        return 
+        return
 
-    def connect(self, address):
+    def connect(self, *args):#assuming both sides know we are go encrpted
+
+        # example code to parse an argument list
+        global local_port_for_sending
+        global in_publicKeys
+        global ENCRYPT
+        global local_box
+        if (len(args) >= 1):
+            (host, port) = args[0]
+        if (len(args) >= 2):
+            if (args[1] == ENCRYPT):
+                self.encrypt = True
+
+                # your code goes here
         print >> sys.stderr, '\nRUNNING instance connect() ... '
         global seq_number
         global ack_number
@@ -56,15 +166,18 @@ class socket(syssock.socket):
         print >> sys.stderr, 'Initial Seq Number: %s' % seq_number
         print >> sys.stderr, 'Initial Ack Number: %s' % ack_number
 
-        #def __prepare_header(self, flags, seq_number, ack_number, payload_len):
+        # def __prepare_header(self, flags, seq_number, ack_number, payload_len):
 
         header = self.__prepare_header(0x01, seq_number, ack_number, 0)
-
+        message = header
+        if self.encrypt == True:
+            header = self.__prepare_header(0x01, seq_number, ack_number, len(publicKeys))
+            message = header + publicKeys
         SYN_acked = False
         while (SYN_acked == False):
-            sock.sendto(header, (local_port_for_sending))
-            print ("Message sent from connect(): %s %s %s %s  %s %s %s %s  %s %s %s %s " 
-                % (struct.unpack('!BBBBHHLLQQLL', header)))
+            sock.sendto(message, (local_port_for_sending))
+            print ("Message sent from connect(): %s %s %s %s  %s %s %s %s  %s %s %s %s "
+                   % (struct.unpack('!BBBBHHLLQQLL', message[0:40])))
             print >> sys.stderr, 'Sending SYN from ... \n', local_port_for_sending
             print >> sys.stderr, 'Block waiting for SYN ACK at ... \n', local_port_for_receiving
 
@@ -79,7 +192,7 @@ class socket(syssock.socket):
             msg_rec = struct.unpack('!BBBBHHLLQQLL', bytesreceived[0:40])
             print >> sys.stderr, 'SYN ACK from addr: ', addr
             print ("SYN ACK Message: %s %s %s %s  %s %s %s %s  %s %s %s %s \n"
-                % (msg_rec) )
+                   % (msg_rec))
             is_syn_ack = False
             is_reset = False
             is_syn_ack = (msg_rec[1] == 0x5)
@@ -93,236 +206,41 @@ class socket(syssock.socket):
                 SYN_acked = True
                 seq_number = msg_rec[9]
                 ack_number = msg_rec[8]
-            #if is_reset == True:
-                #something
-        print >> sys.stderr, 'About to return from connect() ... ' 
-        return 
-
-    def listen(self, backlog):
-        print >> sys.stderr, '\nRUNNING instance listen(self, backlog) .... '
+                if self.encrypt == True:
+                    end_posi_of_key = msg_rec[11]+40
+                    in_publicKeys = bytesreceived[40:end_posi_of_key]
+                    local_box = Box(privateKeys, in_publicKeys)
+                    
+                # if is_reset == True:
+                # something
+        print >> sys.stderr, 'About to return from connect() ... '
         return
 
-    def accept(self):
-        print >> sys.stderr, '\nRUNNING instance accept() ... '
+def listen(self, backlog):
+        # listen is not used in this assignments
+        pass
 
-        addr = self.__sock352_get_packet()
-        print connection_list
-        local_connection_list = connection_list
-        print "socket, before s2=: ", socket()
+    def accept(self, *args):
+        # example code to parse an argument list
+        global ENCRYPT
+        if (len(args) >= 1):
+            if (args[0] == ENCRYPT):
+                self.encryption = True
+                # your code goes here
 
-        global s2
-        s2 = socket()
-
-        print "socket, after s2=: ", socket()
-        print "s2: ", s2
-        global connection_list
-        connection_list = local_connection_list
-        print connection_list, local_connection_list
-
-        return (s2, addr)
-    
     def close(self):
-        sock.close()
+        # your code goes here
         return
-
 
     def send(self, buffer):
-        print >> sys.stderr, '\nRUNNING instance send() ... '
-        bytessent = 0     # fill in your code here 
-        bytesreceived = 0
-
-        print seq_number
-        print ack_number
-        global seq_number
-        global ack_number
-        ack_number = ack_number + 1
-
-        header = self.__prepare_header(0x04, seq_number, ack_number, (len(buffer)))
-        message = header + buffer
-
-        ACK_receipt = False
-        while (not ACK_receipt):
-            sock.sendto(message, (local_port_for_sending))
-
-            sock.setblocking(0)
-            ready = select.select([sock], [], [], 0.2)
-            if ready[0]:
-                bytesreceived, addr = sock.recvfrom(4096)
-
-            if bytesreceived == 0:
-                continue
-
-            msg_rec = struct.unpack('!BBBBHHLLQQLL', bytesreceived[0:40])
-            print >> sys.stderr, 'ACK from addr: ', addr
-            print ("SYN ACK Message: %s %s %s %s  %s %s %s %s  %s %s %s %s \n"
-                   % (msg_rec))
-            is_ack = False
-            is_ack = (msg_rec[1] == 0x4)
-
-            seqMatch = False
-            seqMatch = ((msg_rec[9] == seq_number + 1) & (msg_rec[8] == ack_number))
-
-            print ('is_ack: %s\nseqMatch: %s\n' % (is_ack, seqMatch))
-            if (is_ack == True & seqMatch == True):
-                ACK_receipt = True
-                seq_number = msg_rec[9]
-                ack_number = msg_rec[8]
-
-        print 'len_bytesreceived: %d' %len(buffer)
-        return len(buffer)
+        # your code goes here
+        return
 
     def recv(self, nbytes):
-        global recv_size
-        recv_size = nbytes
-        global fragment_list
-        print >> sys.stderr, '\nRUNNING instance recv() ... '
-        if fragment_list.empty() == False:
-            print "test0"
-            temp = fragment_list.get_nowait()
-            if len(temp) > nbytes:
-                need_bytes = temp[0:nbytes]
-                temp = temp[nbytes:len(temp)]
-                fragment_list.put(temp)
-                return need_bytes
-            else:
-                return temp
-        else:
-            print "test1"
-            addr= self.__sock352_get_packet()
-            temp = fragment_list.get_nowait()
-            need_bytes = temp[0:nbytes]
-            temp = temp[nbytes:len(temp)]
-            fragment_list.put(temp)
-            return need_bytes
-
-
-    def __sock352_get_packet(self):
-        print >> sys.stderr, '\tRUNNING instance __sock352_get_packet() ... '
-        isConnect = False
-        is_syn = False
-        is_ack= False
-        is_fin = False
-        send_reset = False
-        accept_call = False
-        recv_call = False
-        fin_call = False
-        while (isConnect == False):
-            sock.setblocking(0)
-            ready = select.select([sock], [], [], 0.2)
-            if ready[0]:
-                bytesreceived, addr = sock.recvfrom(64000)
-                print len(bytesreceived)
-
-            msg_rec = struct.unpack('!BBBBHHLLQQLL', bytesreceived[0:40])
-            if msg_rec[1] == 0x01:
-                is_syn = True
-            if msg_rec[1] == 0x04:
-                is_ack = True
-            if msg_rec[1] == 0x02:
-                is_fin = True
-
-            if ((is_syn == True) & (len(connection_list) == 0)):
-                isConnect = True
-                accept_call = True
-                break
-            if ((is_syn == True) & len(connection_list) != 0) | ((is_ack == True) & len(connection_list) == 0):
-                isConnect = False
-                send_reset = True
-                break
-            if ((is_ack == True) & len(connection_list) != 0):
-                isConnect = True
-                recv_call = True
-            if (is_fin == True):
-                fin_call = True
-
-        if accept_call == True:
-            global local_seq_number
-            global local_ack_number
-            print ack_number, seq_number
-            local_seq_number = msg_rec[8]
-            global seq_number
-            seq_number = local_seq_number + 1
-            local_seq_number = local_seq_number + 1
-            print "local_seq: ", local_seq_number
-
-            global ack_number
-            ack_number = random.randint(0, 1844674407370955161)
-            local_ack_number = ack_number
-            print "local_ack: ", local_ack_number
-
-            header = self.__prepare_header(0x05, ack_number, seq_number, 0)
-            print ("Package Preview inside get_Pack: %s %s %s %s  %s %s %s %s  %s %s %s %s "
-                   % (struct.unpack('!BBBBHHLLQQLL', header)))
-            sock.sendto(header, (local_port_for_sending))
-
-            global connection_list
-            connection_list.append((seq_number - 1, ack_number))
-
-        if recv_call == True:
-            global local_seq_number
-            global local_ack_number
-            global fragment_list
-            fragment_list.put(bytesreceived[40:len(bytesreceived)])
-
-            print ack_number, seq_number
-            #if (local_seq_number == msg_rec[8] & local_ack_number+1 == msg_rec[9]):
-            local_seq_number = msg_rec[8] + 1
-            local_ack_number = msg_rec[9]
-
-            header = self.__prepare_header(0x04, local_ack_number, local_seq_number, 0)
-            print ("Package Preview inside get_Pack: %s %s %s %s  %s %s %s %s  %s %s %s %s "
-                % (struct.unpack('!BBBBHHLLQQLL', header)))
-            sock.sendto(header, (local_port_for_sending))
-
-
-        # call __sock352_get_packet() to get packets (polling)
-        # check the list of received fragements
-        # copy up to bytes_to_receive into a buffer
-        # return the buffer if there is some data
+        # your code goes here
+        return
 
 
 
-        return addr
 
 
-
-    def __prepare_header(self, flags, seq_number, ack_number, payload_len):
-        print >> sys.stderr, '\tRUNNING instance _prepare_header() ... '
-        sock352PktHdrData = '!BBBBHHLLQQLL'
-        udpPkt_hdr_data = struct.Struct(sock352PktHdrData)
-
-        version = 0x1
-
-        global syn, fin, ack, reset, has_opt
-        syn = 0x01#0x01 | 0x04 = 5
-        fin = 0x02
-        ack = 0x04
-        reset = 0x08
-        has_opt = 0x10
-
-        opt_ptr = 0x0
-        protocol = 0x0
-
-        header_len = 0x28  # H
-        checksum = 0x0
-
-        source_port = 0x0  # L
-        dest_port = 0x0
-
-        window = 0x0  # L
-
-
-        header_packed = udpPkt_hdr_data.pack(version, flags, opt_ptr, protocol,  # B
-                                      header_len, checksum,  # H
-                                      source_port, dest_port,  # L
-                                      seq_number, ack_number,  # Q
-                                      window, payload_len)  # L
-
-        print ("Package Preview: %s %s %s %s  %s %s %s %s  %s %s %s %s "
-       % (struct.unpack('!BBBBHHLLQQLL', header_packed)))
-
-            
-        return header_packed
-
-
-    
